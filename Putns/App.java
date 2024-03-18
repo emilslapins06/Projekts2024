ed019e4bace5a7f4f5d1d984b87820517780463c
22DP3KZari <131261317+22DP3KZari@users.noreply.github.com>
2024-03-18 18:57:04 +0000

    jaunakais commits

--- Putns/App.java
+++ Putns/App.java
@@ -2,11 +2,10 @@ import javax.swing.*;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        new RegistrationWindow();
         int boardWidth = 360;
         int boardHeight = 640;
 
-        JFrame frame = new JFrame("Lidojosais Putnins");
+        JFrame frame = new JFrame("Flappy Bird");
         // frame.setVisible(true);
 		frame.setSize(boardWidth, boardHeight);
         frame.setLocationRelativeTo(null);
